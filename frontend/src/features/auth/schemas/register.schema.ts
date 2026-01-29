36733fb5847a6f9b7120b2f3587b6d53a18bebbf
“farah-jannat” <farahjannatmithila@gmail.com>
2026-01-29 22:06:10 +0600

    feat: add zod schema for register form

---
+++ frontend/src/features/auth/schemas/register.schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  email: z.string().min(1, "Email is required"),
+  phoneNumber: z.string().min(1, "Phone number is required"),
+  password: z.string().min(1, "Password is required"),
+  name: z.string().min(1, "fullname is required"),
+});
+
+export type RegisterSchemaType = z.infer<typeof registerSchema>;
