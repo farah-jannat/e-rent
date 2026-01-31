7a9ee950787e9cd3fdfdc6e89b10a472c7b1711a
“farah-jannat” <farahjannatmithila@gmail.com>
2026-01-31 15:22:24 +0600

    feat: add jwt token in config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,4 +2,5 @@ export const config = {
   DATABASE_URL: process.env.DATABASE_URL || "",
   CLIENT_URL: process.env.CLIENT_URL || "",
   SECRET_KEY_ONE: process.env.SECRET_KEY || "",
+  JWT_TOKEN: process.env.JWT_TOKEN || ""
 };
