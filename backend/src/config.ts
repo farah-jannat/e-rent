1694665039d77ba8e386f46020dccfd508fc779a
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-01 12:48:11 +0600

    feat: add required tokens

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,5 +2,6 @@ export const config = {
   DATABASE_URL: process.env.DATABASE_URL || "",
   CLIENT_URL: process.env.CLIENT_URL || "",
   SECRET_KEY_ONE: process.env.SECRET_KEY || "",
-  JWT_TOKEN: process.env.JWT_TOKEN || ""
+  JWT_TOKEN: process.env.JWT_TOKEN || "",
+  GATEWAY_JWT_TOKEN: process.env.GATEWAY_JWT_TOKEN || "",
 };
