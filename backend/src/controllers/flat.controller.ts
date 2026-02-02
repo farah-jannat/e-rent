4e29750edb96ae487efcf172d4ffa3a43bb491ac
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-02 12:26:42 +0600

    fix: import catchError from fvoid shared-lib

--- backend/src/controllers/flat.controller.ts
+++ backend/src/controllers/flat.controller.ts
@@ -3,11 +3,12 @@ import { flats } from "@/schemas";
 import type { RegisterFlatInput } from "@/validations/flat.validation";
 import { catchError, NotAuthorizedError } from "@fvoid/shared-lib";
 import { and, eq } from "drizzle-orm";
-// import { catchError } from "drizzle/migrate";
 import type { Request, Response } from "express";
 
 export const registerFlat = async (req: Request, res: Response) => {
+  console.log("hello this is form register flat req.body", req.body)
   const landlord = req.landlord;
+  console.log("llndlord ##########################",landlord)
   if (!landlord) throw new NotAuthorizedError();
   const formdata = req.body as RegisterFlatInput;
 
@@ -30,4 +31,11 @@ export const registerFlat = async (req: Request, res: Response) => {
       }),
     );
   }
+  if (flat) {
+    return res.json({
+      message: "Account created successfully",
+    });
+  }
+
+  throw new Error("Db error registerring flat");
 };
