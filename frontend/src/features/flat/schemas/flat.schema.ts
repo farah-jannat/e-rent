69561dadac34d0606ecccab0e2e642a043ae8c5f
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-02 15:26:53 +0600

    feat: add flat zod schema

---
+++ frontend/src/features/flat/schemas/flat.schema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const flatSchema = z.object({
+  id: z
+    .string()
+    .uuid()
+    .describe("Unique identifier for the job, auto-generated UUID."),
+  landlordId: z.string(),
+  name: z.string().min(1, "fullname is required"),
+  baseRent: z.number(),
+  serviceCharge: z.number(),
+  isOccupied: z.boolean(),
+});
+
+export type FlatSchemaType = z.infer<typeof flatSchema>;
