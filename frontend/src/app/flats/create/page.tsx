09ed0aab6dee37121e0fa1ef17ed10adc8bd9475
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-02 13:03:23 +0600

    feat: add create-flat page

---
+++ frontend/src/app/flats/create/page.tsx
@@ -0,0 +1,12 @@
+import FlatRegisterForm from "@/features/flat/components/register-flat-form";
+import React from "react";
+
+const createFlat = () => {
+  return (
+    <div className="w-[50%] mx-[100px] my-[50px]">
+      <FlatRegisterForm />
+    </div>
+  );
+};
+
+export default createFlat;
