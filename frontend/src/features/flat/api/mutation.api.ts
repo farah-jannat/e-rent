e8561027e6fd0057a6a95cf7c5d4a5a09cda7870
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-04 15:34:41 +0600

    fix: fix register type

--- frontend/src/features/flat/api/mutation.api.ts
+++ frontend/src/features/flat/api/mutation.api.ts
@@ -1,11 +1,10 @@
 import { flat__axios } from "@/axios.service";
 import { Flat } from "@/features/flat/schemas/flat.schema";
 import { RegisterFlatSchemaType } from "@/features/flat/schemas/register-flat.schema";
-import { Register } from "@tanstack/react-query";
 
 export const registerFlat = async (data: RegisterFlatSchemaType) => {
   console.log("data from register flat api", data);
-  const response = await flat__axios.post<Register>(`/register`, data);
+  const response = await flat__axios.post(`/register`, data);
   return response.data;
 };
 
