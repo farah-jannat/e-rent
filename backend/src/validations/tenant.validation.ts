1027a1c8d71fc461e14c16e6d4b31e0d4e846fdf
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-17 16:26:53 +0600

    feat: add tennat validation

--- backend/src/validations/tenant.validation.ts
+++ backend/src/validations/tenant.validation.ts
@@ -6,10 +6,24 @@ export const registerTenantSchema = z.object({
   //     .uuid()
   //     .describe("Unique identifier for the job, auto-generated UUID."),
   name: z.string().min(1, "fullname is required"),
-  email:z.string(),
+  email: z.string(),
   phone: z.string(),
   flatId: z.string(),
   landlordId: z.string(),
 });
 
 export type RegisterTenantInput = z.infer<typeof registerTenantSchema>;
+
+export const tenantSchema = z.object({
+  id: z
+    .string()
+    .uuid()
+    .describe("Unique identifier for the job, auto-generated UUID."),
+  name: z.string().min(1, "fullname is required"),
+  phone: z.string(),
+  email: z.string(),
+  landlordId: z.string(),
+  flatId: z.string(),
+});
+
+export type TenantInput = z.infer<typeof tenantSchema>;
