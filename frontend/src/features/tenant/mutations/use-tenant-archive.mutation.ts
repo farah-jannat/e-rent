8dc6d90e9155d31c9957c35a8f7ead43072aa6dc
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-17 19:04:32 +0600

    feat: add queryclient

--- frontend/src/features/tenant/mutations/use-tenant-archive.mutation.ts
+++ frontend/src/features/tenant/mutations/use-tenant-archive.mutation.ts
@@ -1,17 +1,22 @@
 import { archiveTenant } from "@/features/tenant/api/mutation.api";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
-
 const useTenantArchiveMutation = () => {
+  const queryClient = useQueryClient();
   const router = useRouter();
   return useMutation({
-    mutationFn: (id:string) => archiveTenant(id),
+    mutationFn: (id: string) => archiveTenant(id),
     onSuccess() {
       toast.success(`succesfully archived your tenant`);
       router.push(`/tenants`);
+
+      queryClient.invalidateQueries({
+        queryKey: ["tenants"],
+      });
     },
+
     onError() {
       toast.error("Error Editing tenant :( !");
     },
