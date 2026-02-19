6fe08a778dffb9e09af72f52da8944346b722da7
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-19 14:57:02 +0600

    update flat schema

--- backend/src/schemas/flat.schema.ts
+++ backend/src/schemas/flat.schema.ts
@@ -1,11 +1,21 @@
 import { landlords } from "@/schemas/landlord.schema";
-import { pgTable, boolean, numeric, uuid, text } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  boolean,
+  numeric,
+  uuid,
+  text,
+  integer,
+} from "drizzle-orm/pg-core";
 
 export const flats = pgTable("flats", {
   id: uuid("id").defaultRandom().primaryKey(),
   landlordId: uuid("landlord_id").references(() => landlords.id),
   name: text("name").notNull(), // e.g., "Flat 4A"
-  baseRent: numeric("base_rent", { precision: 10, scale: 2 }),
-  serviceCharge: numeric("service_charge", { precision: 10, scale: 2 }).default("0"),
+  // baseRent: numeric("base_rent", { precision: 10, scale: 2 }),
+  baseRent: integer("base_rent"),
+  serviceCharge: numeric("service_charge", { precision: 10, scale: 2 }).default(
+    "0",
+  ),
   isOccupied: boolean("is_occupied").default(false),
 });
--- backend/src/validations/flat.validation.ts
+++ backend/src/validations/flat.validation.ts
@@ -1,12 +1,20 @@
 import { z } from "zod";
 
 export const registerFlatSchema = z.object({
+  // id: z
+  //   .string()
+  //   .uuid()
+  //   .describe("Unique identifier for the job, auto-generated UUID."),
+  // name: z.string().min(1, "fullname is required"),
+  // landlordId: z.string(),
+
   id: z
     .string()
     .uuid()
     .describe("Unique identifier for the job, auto-generated UUID."),
-  name: z.string().min(1, "fullname is required"),
   landlordId: z.string(),
+  name: z.string().min(1, "fullname is required"),
+  baseRent: z.coerce.number().min(1, "Rent must be greater than 0"),
 });
 
 export type RegisterFlatInput = z.infer<typeof registerFlatSchema>;
