c900222196925a6c46daf0ebf64457857ab88513
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-19 14:56:42 +0600

    fix: fix baserent to integer

--- frontend/src/features/flat/schemas/register-flat.schema.ts
+++ frontend/src/features/flat/schemas/register-flat.schema.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
 export const registerFlatSchema = z.object({
-  // id: z
-  //   .string()
-  //   .uuid()
-  //   .describe("Unique identifier for the job, auto-generated UUID."),
-  // landlordId: z.string(),
+  id: z
+    .string()
+    .uuid()
+    .describe("Unique identifier for the job, auto-generated UUID."),
+  landlordId: z.string(),
   name: z.string().min(1, "fullname is required"),
-  //   baseRent:
+  baseRent: z.coerce.number().min(1, "Rent must be greater than 0"),
   //   serviceCharge:
   //   isOccupied:
 });
