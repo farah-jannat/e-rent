e0aade1fd61e5840cc707d69c8915542a228531f
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-19 15:42:50 +0600

    fix: fix flat validation

--- frontend/src/features/flat/schemas/register-flat.schema.ts
+++ frontend/src/features/flat/schemas/register-flat.schema.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
 export const registerFlatSchema = z.object({
-  id: z
-    .string()
-    .uuid()
-    .describe("Unique identifier for the job, auto-generated UUID."),
-  landlordId: z.string(),
+  // id: z
+  //   .string()
+  //   .uuid()
+  //   .describe("Unique identifier for the job, auto-generated UUID."),
+  // landlordId: z.string(),
   name: z.string().min(1, "fullname is required"),
   baseRent: z.coerce.number().min(1, "Rent must be greater than 0"),
   //   serviceCharge:
