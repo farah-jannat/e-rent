b4203c89fcc48ff45775967f39ec9656e78971e1
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-23 19:20:49 +0600

    feat: add quota

--- backend/src/schemas/index.ts
+++ backend/src/schemas/index.ts
@@ -1,8 +1,6 @@
-export * from "@/schemas/landlord.schema"
-export * from "@/schemas/tenant.schema"
-export * from "@/schemas/rentPayment.shcema"
-export * from "@/schemas/flat.schema"
-export * from "@/schemas/subscription.schema"
-
-
-
+export * from "@/schemas/landlord.schema";
+export * from "@/schemas/tenant.schema";
+export * from "@/schemas/rentPayment.shcema";
+export * from "@/schemas/flat.schema";
+export * from "@/schemas/subscription.schema";
+export * from "@/schemas/quota.schema";
---
+++ backend/src/schemas/quota.schema.ts
@@ -0,0 +1,14 @@
+import { landlords } from "@/schemas/landlord.schema";
+import { integer, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const quotas = pgTable("landlords", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  landlordId: uuid("landlord_id").references(() => landlords.id),
+  count: integer("count").default(0).notNull(),
+
+  //   name: text("name").notNull(),
+  //   email: text("email").unique().notNull(),
+  //   password: text("password").notNull(),
+  //   phoneNumber: text("phone_number"),
+  //   createdAt: timestamp("created_at").defaultNow(),
+});
