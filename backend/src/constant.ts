13d4a1b97ac37fe4e6480071bca9c3a342f52bc5
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-23 19:21:01 +0600

    feat: add constant ts

---
+++ backend/src/constant.ts
@@ -0,0 +1,14 @@
+export const Free = {
+  flats: 2,
+  price: 0,
+} as const;
+
+export const Standard = {
+  flats: 10,
+  price: 500,
+} as const;
+
+export const Premium = {
+  flats: 2147483647,
+  price: 1500,
+} as const;
