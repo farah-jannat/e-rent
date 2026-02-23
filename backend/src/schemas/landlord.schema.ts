e8a9a218b13551631b85b9802efd0cdeac714dc6
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-23 19:21:48 +0600

    feat: add plan

--- backend/src/schemas/landlord.schema.ts
+++ backend/src/schemas/landlord.schema.ts
@@ -1,10 +1,24 @@
+import { Free } from "@/constant";
 import { flats } from "@/schemas/flat.schema";
+import { quotas } from "@/schemas/quota.schema";
 import { rentPayment } from "@/schemas/rentPayment.shcema";
 import { tenants } from "@/schemas/tenant.schema";
 import { relations } from "drizzle-orm";
-import { pgTable, timestamp, uuid, text } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  timestamp,
+  uuid,
+  text,
+  integer,
+  pgEnum,
+} from "drizzle-orm/pg-core";
 
 // --- * landlord Table *-------
+export const planEnum = pgEnum("landlord_plans", [
+  "free",
+  "standard",
+  "premium",
+]);
 
 export const landlords = pgTable("landlords", {
   id: uuid("id").defaultRandom().primaryKey(),
@@ -12,11 +26,14 @@ export const landlords = pgTable("landlords", {
   email: text("email").unique().notNull(),
   password: text("password").notNull(),
   phoneNumber: text("phone_number"),
+  quotaLimit: integer("quota_limit").default(Free.flats).notNull(),
+  plan: planEnum("plan").notNull().default("free"),
   createdAt: timestamp("created_at").defaultNow(),
 });
 
-export const landlordRelations = relations(landlords, ({ many }) => ({
+export const landlordRelations = relations(landlords, ({ many, one }) => ({
   flats: many(flats),
   tenants: many(tenants),
   payments: many(rentPayment),
+  // quota: one(quotas)
 }));
