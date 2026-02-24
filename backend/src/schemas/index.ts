1ff49b93c39cacbfb2f35cbd37d0ce7f67e4e4e0
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-24 15:54:44 +0600

    fix: comment out unrequired schema

--- backend/src/schemas/index.ts
+++ backend/src/schemas/index.ts
@@ -1,6 +1,6 @@
 export * from "@/schemas/landlord.schema";
 export * from "@/schemas/tenant.schema";
-export * from "@/schemas/rentPayment.shcema";
+export * from "@/schemas/rentPayment.schema";
 export * from "@/schemas/flat.schema";
-export * from "@/schemas/subscription.schema";
+// export * from "@/schemas/subscription.schema";
 export * from "@/schemas/quota.schema";
