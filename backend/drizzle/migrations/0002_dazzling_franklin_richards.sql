c5b94c584dfc3c078cab44c9a66c42cc8b96151e
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-24 15:53:49 +0600

    chore: delete unnessery fles

--- backend/drizzle/migrations/0002_dazzling_franklin_richards.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "flats" ALTER COLUMN "service_charge" SET DATA TYPE integer;--> statement-breakpoint
-ALTER TABLE "flats" ALTER COLUMN "service_charge" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "rent_payments" ALTER COLUMN "amount_due" SET DATA TYPE integer;--> statement-breakpoint
-ALTER TABLE "rent_payments" ALTER COLUMN "amount_due" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "rent_payments" ALTER COLUMN "amount_paid" SET DATA TYPE integer;--> statement-breakpoint
-ALTER TABLE "rent_payments" ALTER COLUMN "amount_paid" DROP DEFAULT;
