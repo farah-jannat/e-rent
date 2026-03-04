6a4439bec21f7872c757f810d39dd9d1d9dde579
asifulhaque087 <asifulhaque087@gmail.com>
2026-03-05 00:36:38 +0600

    feat: update subscription validation input

--- backend/src/validations/subscription.validation.ts
+++ backend/src/validations/subscription.validation.ts
@@ -1,10 +1,12 @@
 import { z } from "zod";
 
-export const planEnumSchema = z.enum(["free", "standard", "premium"]);
-
+export const planEnumSchema = z.enum(["standard", "premium"]);
 
 export const subscriptionSchema = z.object({
-  plan: planEnumSchema.default("free"),
+  plan: planEnumSchema,
+  // duration: z.int,
+  // duration: z.coerce.number().min(1, "Duration must be greater than 0"),
+  durationMonths: z.union([z.literal(1), z.literal(6), z.literal(12), z.literal(24)]),
 });
 
 export type SubscriptionInput = z.infer<typeof subscriptionSchema>;
